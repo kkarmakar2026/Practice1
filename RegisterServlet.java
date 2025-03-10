1267232122b51ddd43c2362c6557b80d3bd831a4
kkarmakar2026 <132549270+kkarmakar2026@users.noreply.github.com>
2025-03-10 16:35:20 +0530

    Update RegisterServlet.java

--- RegisterServlet.java
+++ RegisterServlet.java
@@ -1,1 +1,38 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        String email = request.getParameter("email");
+        String gender = request.getParameter("gender");
+        String service = request.getParameter("services");
+        String subscribe = request.getParameter("subscribe") != null ? "Yes" : "No";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/company_db", "root", "password");
+            PreparedStatement ps = con.prepareStatement("INSERT INTO users (name, email, gender, service, subscribe) VALUES (?, ?, ?, ?, ?)");
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, gender);
+            ps.setString(4, service);
+            ps.setString(5, subscribe);
+            ps.executeUpdate();
+            response.getWriter().println("Registration Successful!");
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.getWriter().println("Error: " + e.getMessage());
+        }
+    }
+}
+
 
